8f2bb02c0b208c84ba0439f89139381379bff7fb
Peacock <M2300497@middlesbro.ac.uk>
2025-10-02 13:36:12 +0100

    added enemy AI and temp asset

---
+++ scripts/ai/mobile_enemy.gd
@@ -0,0 +1,22 @@
+extends RigidBody2D
+
+var direction = 1
+var last_turn = 0
+@export var wandering_dist = 50
+@export var speed = 20
+
+func _ready():
+	last_turn = Time.get_ticks_msec()
+	
+func _physics_process(delta):
+	position.x += direction * speed * delta
+	
+	var current_time = Time.get_ticks_msec()
+	if (current_time - last_turn > wandering_dist):
+		last_turn = current_time
+		direction *= -1
+	
+	
+
+func _despawn():
+	queue_free()
